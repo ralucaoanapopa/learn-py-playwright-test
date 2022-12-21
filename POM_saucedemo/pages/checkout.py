fa572d9cf4fe43877897c3b13f20c41968da112c
Raluca Popa <popa.raluca.oana@gmail.com>
2022-12-21 17:16:05 +0200

    more checkout tests

--- POM_saucedemo/pages/checkout.py
+++ POM_saucedemo/pages/checkout.py
@@ -14,6 +14,26 @@ class CheckoutPage(BasePage):
     item_price_class = ".inventory_item_price"
     onesie_remove_id = "#remove-sauce-labs-onesie"
 
+    first_name_id = "#first-name"
+    last_name_id = "#last-name"
+    postal_code_id = "#postal-code"
+    continue_checkout_button_id = "#continue"
+
+    cancel_button_id = "#cancel"
+    summary_info_class = ".summary_info_label"
+    summary_value_class = ".summary_value_label"
+    summary_subtotal_class= ".summary_subtotal_label"
+    summary_tax_class= ".summary_tax_label"
+    summary_total_class= ".summary_total_label"
+
+    finish_checkout_button_id = "#finish"
+    complete_header_class = ".complete-header"
+    complete_text_class = ".complete-text"
+    final_image_class = ".pony_express"
+    back_button_id = "#back-to-products"
+
+    error_validation_class = ".error-message-container.error"
+
     def __init__(self, page: Page):
         super().__init__()
         self.page = page
@@ -48,3 +68,41 @@ class CheckoutPage(BasePage):
     def remove_product(self, product_id):
         product_to_remove = self.page.locator(product_id)
         product_to_remove.click()
+
+    def click_checkout(self):
+        self.page.locator(self.checkout_button_id).click()
+
+    def enter_first_name(self, first):
+        self.page.locator(self.first_name_id).fill(first)
+    
+    def enter_last_name(self, last):
+        self.page.locator(self.last_name_id).fill(last)
+
+    def enter_postal_code(self, code):
+        self.page.locator(self.postal_code_id).fill(code)
+
+    def enter_checkout_info(self, first, last, code):
+        self.enter_first_name(first)
+        self.enter_last_name(last)
+        self.enter_postal_code(code)
+
+    def get_button_continue_checkout(self):
+        return self.page.locator(self.continue_checkout_button_id)
+
+    def click_continue(self):
+        self.get_button_continue_checkout().click()
+
+    def get_button_cancel(self):
+        return self.page.locator(self.cancel_button_id)
+
+    def get_button_finish(self):
+        return self.page.locator(self.finish_checkout_button_id)
+
+    def elements_by_class(self, element):
+        return self.page.locator(element)
+    
+    def click_finish(self):
+        self.get_button_finish().click()
+
+    def click_back(self):
+        self.page.locator(self.back_button_id).click()
--- POM_saucedemo/test_checkout.py
+++ POM_saucedemo/test_checkout.py
@@ -70,7 +70,7 @@ def test_should_be_able_to_see_shopping_cart_content(before_all_after_all):
     all_items_prices = checkout_page.get_items_price_list()
     expect(all_items_prices.nth(0)).to_have_text(product_Onesie_price)
     expect(all_items_prices.nth(1)).to_have_text(product_BoltTShirt_price)
-    expect(all_items_prices.nth(2)).to_have_text(product_Backpack_Price)
+    expect(all_items_prices.nth(2)).to_have_text(product_Backpack_price)
 
 
 def test_should_be_able_to_navigate_to_inventory_page(before_all_after_all):
@@ -109,6 +109,136 @@ def test_should_be_able_to_remove_product_from_cart(before_all_after_all):
 
     checkout_page.remove_product(checkout_page.onesie_remove_id)
     expect(checkout_page.get_cart_items_list()).to_have_count(3)
+
+
+def test_should_be_able_to_provide_information_on_checkout(before_all_after_all):
+    page = before_all_after_all
+
+    checkout_page = CheckoutPage(page)
+
+    checkout_page.click_checkout()
+
+    expect(page).to_have_url(checkout_page.checkout_one_url)
+    expect(checkout_page.get_title_checkout()).to_have_text(checkout_one_title)
+    checkout_page.enter_checkout_info(first_name, last_name, postal_code)
+    expect(checkout_page.get_button_continue_checkout()).to_have_text(continue_checkout)
+
+
+def test_should_be_able_to_see_overview_on_checkout(before_all_after_all):
+    page = before_all_after_all
+
+    checkout_page = CheckoutPage(page)
+
+    checkout_page.click_continue()
+    expect(page).to_have_url(checkout_page.checkout_two_url)
+    expect(checkout_page.get_title_checkout()).to_have_text(checkout_two_title)
+    expect(checkout_page.get_cart_quantity()).to_have_text(cart_quantity)
+    expect(checkout_page.get_cart_description()).to_have_text(cart_description)
+
+    expect(checkout_page.get_button_cancel()).to_have_text(cancel_checkout)
+    expect(checkout_page.get_button_finish()).to_have_text(finish_checkout)
+
+    expect(checkout_page.get_cart_items_list()).to_have_count(3)
+    all_items = checkout_page.get_cart_items_list()
+
+    expect(all_items.nth(0)).to_contain_text(product_BoltTShirt)
+    expect(all_items.nth(1)).to_contain_text(product_Backpack)
+    expect(all_items.nth(2)).to_contain_text(product_Jacket)
+
+    expect(checkout_page.get_items_price_list()).to_have_count(3)
+    all_items_prices = checkout_page.get_items_price_list()
+    expect(all_items_prices.nth(0)).to_have_text(product_BoltTShirt_price)
+    expect(all_items_prices.nth(1)).to_have_text(product_Backpack_price)
+    expect(all_items_prices.nth(2)).to_have_text(product_FleeceJacket_price)
+
+
+def test_should_be_able_to_see_total_price_and_shipping_information_on_checkout(before_all_after_all):
+    page = before_all_after_all
+
+    checkout_page = CheckoutPage(page)
+
+    expect(page).to_have_url(checkout_page.checkout_two_url)
+    all_summary_info = checkout_page.elements_by_class(checkout_page.summary_info_class)
+    expect(all_summary_info.nth(0)).to_have_text(payment_information_label)
+    expect(all_summary_info.nth(1)).to_have_text(shipping_information_label)
+
+    all_summary_values = checkout_page.elements_by_class(checkout_page.summary_value_class)
+    expect(all_summary_values.nth(0)).to_have_text(payment_information_value)
+    expect(all_summary_values.nth(1)).to_have_text(shipping_information_value)
+
+    expect(checkout_page.elements_by_class(checkout_page.summary_subtotal_class)).to_have_text(total_item_price)
+    expect(checkout_page.elements_by_class(checkout_page.summary_tax_class)).to_have_text(tax_price)
+    expect(checkout_page.elements_by_class(checkout_page.summary_total_class)).to_have_text(total_price)
+
+
+def test_should_be_able_to_finish_order_from_checkout(before_all_after_all):
+    page = before_all_after_all
+
+    checkout_page = CheckoutPage(page)
+
+    checkout_page.click_finish()
+    expect(page).to_have_url(checkout_page.checkout_final_url)
+    expect(checkout_page.get_title_checkout()).to_have_text(checkout_final_title)
+
+    expect(checkout_page.elements_by_class(checkout_page.complete_header_class)).to_have_text(complete_header)
+    expect(checkout_page.elements_by_class(checkout_page.complete_text_class)).to_have_text(complete_text)
+    expect(checkout_page.elements_by_class(checkout_page.final_image_class)).to_have_attribute("src", final_img)
+
+
+def test_should_be_able_to_navigate_back_to_inventory_after_order_was_sent(before_all_after_all):
+    page = before_all_after_all
+
+    checkout_page = CheckoutPage(page)
+
+    checkout_page.click_back()
+    expect(page).to_have_url(checkout_page.inventory_url)
+    
+
+def test_should_not_be_able_to_navigate_back_to_send_order_without_any_products(before_all_after_all):
+    page = before_all_after_all
+
+    inventory_page = InventoryPage(page)
+    checkout_page = CheckoutPage(page)
+
+    inventory_page.click_shopping_cart()
+    expect(page).to_have_url(inventory_page.cart_url)
+
+    checkout_page.click_checkout()
+    expect(page).to_have_url(checkout_page.checkout_one_url)
+
+    checkout_page.enter_checkout_info(first_name, last_name, postal_code)
+    checkout_page.click_continue()
+    expect(page).to_have_url(checkout_page.checkout_two_url)
+
+    checkout_page.click_finish()
+    expect(page).to_have_url(checkout_page.checkout_final_url)
+
+    pytest.fail(f"Should not be able to sent order without any products")
+
+def test_should_not_be_able_to_see_overview_without_providing_shipping_information(before_all_after_all):
+    page = before_all_after_all
+
+    inventory_page = InventoryPage(page)
+    checkout_page = CheckoutPage(page)
+
+    page.goto(checkout_page.checkout_one_url)
+    expect(page).to_have_url(checkout_page.checkout_one_url)
+
+    checkout_page.click_continue()
+    element_err = checkout_page.elements_by_class(checkout_page.error_validation_class)
+    expect(element_err).to_have_text(error_checkout_first_name)
+
+    checkout_page.enter_first_name(first_name)
+    checkout_page.click_continue()
+    expect(element_err).to_have_text(error_checkout_last_name)
+
+    checkout_page.enter_last_name(last_name)
+    checkout_page.click_continue()
+    expect(element_err).to_have_text(error_checkout_postal_code)
+
+    checkout_page.enter_postal_code(postal_code)
+    checkout_page.click_continue()
+    expect(page).to_have_url(checkout_page.checkout_two_url)
 
     inventory_page.logout_user()
     expect(page).to_have_url(inventory_page.base_url)
